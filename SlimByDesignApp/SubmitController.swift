c59d2536f782654d23a509e827b04f44c1be362d
luisplaz <luisplaz@hotmail.com>
2017-01-25 22:30:51 -0500

    Add images and buttons

---
+++ SlimByDesignApp/SubmitController.swift
@@ -0,0 +1,13 @@
+//
+//  SubmitController.swift
+//  SlimByDesignApp
+//
+//  Created by Luis Plaz on 12/2/16.
+//  Copyright © 2016 Luis Plaz. All rights reserved.
+//
+
+import Cocoa
+
+class SubmitController: UIViewController {
+
+}
