f54101b31a1c1fa9afa19769cdc013af06e7b736
luisplaz <luisplaz@hotmail.com>
2017-01-25 22:34:32 -0500

    Update Misc Code (Done Autom By Xcode)

---
+++ SlimByDesignApp/RestaurantDetailVC.swift
@@ -0,0 +1,13 @@
+//
+//  RestaurantDetailVC.swift
+//  SlimByDesignApp
+//
+//  Created by Luis Plaz on 11/14/16.
+//  Copyright © 2016 Luis Plaz. All rights reserved.
+//
+
+import Cocoa
+
+class RestaurantDetailVC: UIViewController {
+
+}
